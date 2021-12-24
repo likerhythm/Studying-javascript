3cefb36212b80656b2425ab23e9c85a9f62e26bb
likerhythm <wjdalsdl0412@gmail.com>
2021-12-24 14:26:41 +0000

    login - 3: lecture-4.4, hidden, backtick

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-//html 문서에서 login-form을 id로 가진 div를 js로 가져온다
-const loginForm = document.getElementById("login-form");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-//login-form에서 input 태그를 가져온다.
-//document.querySelector("#login-form input")으로 대체 가능
-const loginInput = loginForm.querySelector("input");
+const HIDDEN_CLASSNAME = "hidden";
 
-//인자로 전달되는 요소를 event 변수로 잡음
-function loginSubmit(event) {
-	//새로고침을 막아줌
-	event.preventDefault();
-	console.log(event);
+function onLoginSubmit(event) {
+  	event.preventDefault();
+	//loginForm 안보이게 하기
+  	loginForm.classList.add(HIDDEN_CLASSNAME);
+  	const username = loginInput.value;
+  	console.log(username);
+
+	//`Hello ${username}` == "Hello" + username
+  	greeting.innerText = `Hello ${username}`;
+	//인사말 보이게 하기
+  	greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", loginSubmit);
-
-
-//브라우저는 submit 동작을 하면 자동으로 새로고침 되도록 설계돼있음
-//또한 어떤 이벤트가 발생 했을 때 submit되는 요소를 자동으로 인자로 전달함(코드에서 event 변수)
-//새로고침 되는 것을 막아야함
-//새로고침이 되는 event가 submit이므로 loginForm에서 submit 이벤트를 감지해야함
-//정리하면 loginForm에서 submit 이벤트를 감지하면 새로고침 되는 것을 막고 전달되는 event인자를 잡는다.
+loginForm.addEventListener("submit", onLoginSubmit);
