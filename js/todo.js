b5f18c8d3d8610ac9cb106431c65904d4538baee
likerhythm <wjdalsdl0412@gmail.com>
2021-12-28 14:06:15 +0000

    toDoList append and delete, not save

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,30 @@
 const toDoForm = document.querySelector("#todo-form");
-const toDoInput = todoForm.querySelector("input");
+const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
+
+function deleteToDo(event){
+	event.preventDefault();
+	const li = event.target.parentElement;
+	li.remove();
+}
+
+function paintToDo(newTodo){
+	const li = document.createElement("li");
+	const span = document.createElement("span");
+	const button = document.createElement("button");
+	span.innerText = newTodo;
+	button.innerText = "X";
+	button.addEventListener("click", deleteToDo);
+	li.appendChild(span);
+	li.appendChild(button);
+	toDoList.appendChild(li);
+}
 
 function handleToDoSubmit(event){
 	event.preventDefault();
 	const newToDo = toDoInput.value;
 	toDoInput.value="";
-	toD
+	paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
