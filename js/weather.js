e2f8db1f961eb14007a4b83a2dccfb968a5b4579
likerhythm <wjdalsdl0412@gmail.com>
2022-01-02 05:44:50 +0000

    using weather API and fetch

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = "dcc9e1bf3d19bf54821e3175107d2af0";
+const city = document.querySelector("#weather span:first-child");
+const weather = document.querySelector("#weather span:last-child");
+
 function onGeoOk(position){
-	latitude = position.coords.latitude;
-	longitude = position.coords.longitude;
-	console.log("your location is ", latitude, longitude);
+	lat = position.coords.latitude;
+	lon = position.coords.longitude;
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+		city.innerText = data.name;
+		weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
+	});
 }
 function onGeoError(){
 	alert("Can't find you. No weather for you");
