59692738f49759551ff64bc0fc0fc2ae017dc8e8
likerhythm <wjdalsdl0412@gmail.com>
2022-01-05 10:36:39 +0000

    my_todoList: finished my_clock

--- my_todoList/js/my_todoList.js
+++ my_todoList/js/my_todoList.js
@@ -3,14 +3,17 @@ const todoListSubmitForm = document.querySelector("#todoList-SubmitForm");
 const todoInput = todoListSubmitForm.querySelector("input");
 let todos = [];
  
+//todos배열을 localStorage에 JSON.stringify함수 형태로 저장
 function saveTodoList(){
 	localStorage.setItem("todo", JSON.stringify(todos));
 }
 
+//자바스크립트 상에 요소 만들어서 브라우저에 추가 
 function paintTodo(newTodoObj){
 	const li = document.createElement("li");
 	const span = document.createElement("span");
 	const button = document.createElement("button");
+	button.addEventListener("click", deleteTodo);
 	li.id = newTodoObj.id;
 	span.innerText = newTodoObj.element;
 	button.innerText = "X";
@@ -19,6 +22,7 @@ function paintTodo(newTodoObj){
 	todoList.appendChild(li);
 }
 
+//입력 된 todoList를 obj로 가공하여 추가 및 저장
 function handleTodoSubmit(event){
 	event.preventDefault();
 	console.log(event);
@@ -33,7 +37,21 @@ function handleTodoSubmit(event){
 	saveTodoList();
 }
 
+//button 클릭 됐을 때 해당 버튼의 상위요소 즉 리스트 하나 삭제
+function deleteTodo(event){
+	const li = event.target.parentElement;
+	li.remove();
+	todos = todos.filter((item) => item.id !== parseInt(li.id));
+	saveTodoList();
+}
 
 
+todoListSubmitForm.addEventListener("submit", handleTodoSubmit);
 
-todoListSubmitForm.addEventListener("submit", handleTodoSubmit)
+//새로고침 해도 리스트가 화면 상에 표시되도록 함
+const savedTodo = localStorage.getItem("todo");
+if (savedTodo !== null){
+	const parsedTodo = JSON.parse(savedTodo);
+	todos = parsedTodo;
+	parsedTodo.forEach(paintTodo);
+}
